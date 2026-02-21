fad54b11159e03384af7b12838f84ebfcc492063
redzxx <24050131@hdu.edu.cn>
2026-02-21 18:27:26 +0800

    try

---
+++ tmp/t2.cpp
@@ -0,0 +1,11 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cerr<<#x<<'='<<x<<endl;
+const int maxn=1e6+10;
+int main(){
+    int n;
+    cin>>n;
+    while (n--){
+        
+    }
+}
